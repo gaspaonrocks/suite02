a7503951c116adabe42a76c3d6a7e25220a46d7c
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-03-16 11:56:47 +0100

    résolution exo 3

--- ex03.js
+++ ex03.js
@@ -21,3 +21,42 @@
 */
 
 // write your code below this comment
+
+function randomFloatNum() {
+    var num = Math.random() * 100 + 1;
+    return num.toFixed(2);
+}
+
+function randomIntNum() {
+    var num = parseInt(Math.random() * 100 + 1);
+    return num;
+}
+
+function numberToPrice(nb) {
+    var price = parseFloat(nb).toFixed(2);
+    return price;
+}
+
+function priceToNumber(price) {
+    var nb = parseFloat(price).toFixed(0);
+    return nb;
+}
+
+function checkInput(input) {
+    if (input == null) {
+        return "not a number, fella...";
+    } else if (input == 0) {
+        return "It's either free or empty. Which is it ?"
+    } else if (Number.isInteger(input) == true) {
+        var result = numberToPrice(input);
+        return result + "€";
+    } else if (Number.isInteger(input) == false) {
+        var result = priceToNumber(input);
+        return result;
+    }
+}
+
+console.log(checkInput(randomFloatNum()));
+console.log(checkInput(randomIntNum()));
+console.log(checkInput());
+console.log(checkInput(0));
