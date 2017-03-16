a360f7686b7358f57a80b581e412199365be2381
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-03-16 10:56:45 +0100

    résolution exo 1

--- ex01.js
+++ ex01.js
@@ -27,3 +27,50 @@
 */
 
 // write your code below this comment
+
+function add(nb1, nb2) {
+    return result = nb1 + nb2;
+}
+
+function substract(nb1, nb2) {
+    return result = nb1 - nb2;
+}
+
+function multiply(nb1, nb2) {
+    return result = nb1 * nb2;
+}
+
+function divide(nb1, nb2) {
+    if (nb2 == 0) {
+        console.log(("division is impossible"));
+        return 0;
+    } else {
+        return parseInt(result = nb1 / nb2);
+    }
+}
+
+function getRemainder(nb1, nb2) {
+    return result = nb1 % nb2;
+}
+
+function evalExpr(nb1, nb2, operator) {
+    if (operator == "+") {
+        return add(nb1, nb2);
+    } else if (operator == "-") {
+        return substract(nb1, nb2);
+    } else if (operator == "*") {
+        return multiply(nb1, nb2);
+    } else if (operator == "/") {
+        return divide(nb1, nb2);
+    } else if (operator == "%") {
+        return getRemainder(nb1, nb2);
+    }
+}
+
+console.log(evalExpr(136, 212, "*")); // 28832
+console.log(evalExpr(22, 56, "+")); // 6
+console.log(evalExpr(8, 3, "%")); // 2
+console.log(evalExpr(2, 3, "-")); // -1
+console.log(evalExpr(234, 76, "/")); // 3
+
+console.log(evalExpr(10, 0, "/")); // 0
