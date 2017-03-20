27103f99c92ec6403d0d789e9b7fb45c3a86d084
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-03-20 12:20:07 +0100

    resolution exo 6

--- ex06.js
+++ ex06.js
@@ -1,4 +1,3 @@
-
 /*
 
     Bubble sort
@@ -24,3 +23,21 @@
 
 // write your code below this comment
 
+function bubbleSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        var x = array[i];
+        var j = i - 1;
+        while (j >= 0 && array[j] > x) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = x;
+    }
+    return array;
+}
+
+console.log(bubbleSort([8, 3, 0]));
+console.log(bubbleSort([0, 5, 3, 4, 2, 1]));
+console.log(bubbleSort([1, 5, 3, 2, 4, 0]));
+console.log(bubbleSort([3, 4, 2, 5, 1, 0]));
+console.log(bubbleSort([2, 5, 3, 4, 0, 1]));
